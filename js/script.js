87fb7aed1ddae926ca1a70816225c71e95b4fe23
Benjamin <br.pinseel@gmail.com>
2019-10-21 19:42:58 +0200

    Add visibility functionality

--- js/script.js
+++ js/script.js
@@ -23,26 +23,87 @@ function Initialiseer() {
 
 function KoppelDomElementen() {
       // Labels
-
+      lblOmschrijving1 = document.getElementById(`lblOmschrijving1`);
+      lblOmschrijving2 = document.getElementById(`lblOmschrijving2`)
       // Invoervelden
-
+      txtInput1 = document.getElementById(`txtInput1`);
+      txtInput2 = document.getElementById(`txtInput2`);
       // Headers
-
+      hdrFunctionaliteit = document.getElementById(`hdrFunctionaliteit`);
       // Divisies
-
+      divFeedBack = document.getElementById(`divFeedBack`);
       // Buttons
+      btnDieren = document.getElementById(`btnDieren`);
+      btnWinstVerlies = document.getElementById(`btnWinstVerlies`);
+      btnTafel = document.getElementById(`btnTafel`);
+      btnTafels = document.getElementById(`btnTafels`);
+      btnVoerUit = document.getElementById(`btnVoerUit`);
 
 }
 
 function VoegEventHandlersToe() {
+      btnWinstVerlies.addEventListener(`mouseenter`,function(){ToonFunctionaliteit(winstOfVerlies)});
+      btnDieren.addEventListener(`mouseenter`,function(){ToonFunctionaliteit(dieren)});
+      btnTafel.addEventListener(`mouseenter`,function(){ToonFunctionaliteit(tafel)});
+      btnTafels.addEventListener(`mouseenter`,function(){ToonFunctionaliteit(tafelVanTot)});
+
+      btnWinstVerlies.addEventListener(`click`,);
+      btnDieren.addEventListener(`click`,);
+      btnTafel.addEventListener(`click`,);
+      btnTafels.addEventListener(`click`,);
 
 }
 
 function VerbergElementen() {
+      hdrFunctionaliteit.style.visibility = `hidden`;
+      lblOmschrijving1.style.visibility = `hidden`;
+      lblOmschrijving2.style.visibility = `hidden`;
+      txtInput1.style.visibility = `hidden`;
+      txtInput2.style.visibility = `hidden`;
+      btnVoerUit.style.visibility = `hidden`;
+      divFeedBack.style.visibility = `hidden`;
+
 
 }
 
-function ToonFunctionaliteit() {
+function ToonFunctionaliteit(titel) {
+      hdrFunctionaliteit.style.visibility = `visible`;
+
+      txtInput1.value = ``;
+      txtInput2.value = ``;
+      divFeedBack.innerHTML = ``;
+      hdrFunctionaliteit.innerHTML = titel;
+
+      switch(titel){
+            case winstOfVerlies:
+                  lblOmschrijving1.innerHTML = `Aankoopprijs`;
+                  lblOmschrijving1.style.visibility = `visible`;
+                  lblOmschrijving2.innerHTML = `Verkoopprijs`;
+                  lblOmschrijving2.style.visibility = `visible`;
+                  txtInput2.style.visibility = `visible`
+                  break;
+            case dieren:
+                  lblOmschrijving1.innerHTML = `Aantal huisdieren`;
+                  lblOmschrijving1.style.visibility = `visible`;
+                  lblOmschrijving2.style.visibility = `hidden`;
+                  txtInput2.style.visibility = `hidden`;
+                  break;
+            case tafel:
+                  lblOmschrijving1.innerHTML = `Tafel van`
+                  lblOmschrijving1.style.visibility = `visible`;
+                  lblOmschrijving2.style.visibility = `hidden`;
+                  txtInput2.style.visibility = `hidden`;
+                  break;
+            case tafelVanTot:
+                  lblOmschrijving1.innerHTML = `Tafel van`;
+                  lblOmschrijving1.style.visibility = `visible`;
+                  lblOmschrijving2.innerHTML = `tot`;
+                  txtInput2.style.visibility = `visible`
+                  lblOmschrijving2.style.visibility = `visible`;
+                  break;
+      }
+
+
    
 }
 
